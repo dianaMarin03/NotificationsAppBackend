0970a174feda5e43763cf7ae459d764e1e9ac1e6
Diana Marin <dianamarin724@yahoo.com>
2024-03-18 18:05:09 +0200

    add commit

---
+++ NewsAPI/Services/ICollectionService.cs
@@ -0,0 +1,19 @@
+﻿using NewsAPI.Models;
+//doar declari functiile cu tipul lor 
+namespace NewsAPI.Services
+{
+    public interface ICollectionService<T> where T : new()
+
+    {
+        Task <List<Announcement>> GetAll();
+
+        Task <Announcement> Get(Guid id);
+
+       Task< bool> Create(T model);
+
+       Task< bool> Update(Guid id, T model);
+
+       Task< bool> Delete(Guid id);
+
+    }
+}
